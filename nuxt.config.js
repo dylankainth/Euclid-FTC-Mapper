7bf4a7ee896876b1c7c39516cc90e7c006c45a46
Dylan K <kainth.dylan@gmail.com>
2023-03-28 00:49:45 +0100

    second commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,20 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css',
+        rel: 'stylesheet',
+        integrity:
+          'sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor',
+        crossorigin: 'anonymous',
+      },
+    ],
+    script: [ {
+      src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js',
+      integrity:
+        'sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2',
+      crossorigin: 'anonymous',
+    }
     ]
   },
 
@@ -43,7 +57,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: '/.netlify/functions/',
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,51 @@
 <template>
-  <div id="map-wrap" style="height: 100vh">
- <client-only>
-   <l-map :zoom=13 :center="[55.9464418,8.1277591]">
-     <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-     <l-marker :lat-lng="[55.9464418,8.1277591]"></l-marker>
-   </l-map>
- </client-only>
-</div>
+  <div>
+    <center><h1>Team Euclid FTC Team Mapper</h1></center>
+
+    <div id="map-wrap" style="height: 80vh">
+      <client-only>
+        <l-map :zoom="2" :center="[0, 0]">
+          <l-tile-layer
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          ></l-tile-layer>
+
+          <div v-for="pin in pins" :key="pin.id">
+            <l-marker :lat-lng="[pin.team_location.lat, pin.team_location.lng]">
+              <l-popup>
+                <div>
+                  Team
+                  <a
+                    :href="
+                      'https://ftc-events.firstinspires.org/team/' +
+                      pin.team_number_yearly
+                    "
+                    target="_blank"
+                  >
+                    <b>{{ pin.team_number_yearly }}</b>
+                  </a>
+                </div>
+              </l-popup>
+            </l-marker>
+          </div>
+        </l-map>
+      </client-only>
+    </div>
+
+    <div class="container">
+    <p class="px-5 pt-2" >Here's a quick mockup for the basic features of this website. It is not styled, so it doesn't yet look pretty or brand-consistent. Making a CSS Stylesheet to improve the look and feel of the website is next. This will include colours, images and user interface design</p>
+  
+    <p class="px-5">Also, teams at null island have dodgy data, so that needs to be fixed too.</p>
+
+    <p class="px-5"><a href="https://dylankainth.com">~ Dylan</a></p>
+  </div>
+  </div>
 </template>
 
 <script>
 export default {
-  
- 
+  async asyncData({ $axios }) {
+    const pins = await $axios.$get('getMapPins')
+    return { pins }
+  },
 }
-</script> 
+</script>
